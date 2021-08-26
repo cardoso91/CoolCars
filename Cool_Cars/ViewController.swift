73fab7e4079357ac6adf9e7277bb6b0ec8cc9d69
Carlos Cardoso <carlos.cardoso91@gmail.com>
2021-08-26 10:37:35 -0700

    Initial Commit

---
+++ Cool_Cars/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cool_Cars
+//
+//  Created by Carlos Cardoso on 8/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
